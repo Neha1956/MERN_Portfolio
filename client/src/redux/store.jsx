1d212d09fe7dd545972ffc672771ff738f3774a0
Neha <nehagarhwa5@gmail.com>
2026-05-10 20:29:51 +0530

    fix redux import case issue

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectReducer from "./projectSlice";
 import authReducer from "./authSlice";
-import ProfileReducer from "./ProfileSlice";
+import ProfileReducer from "./profileSlice";
 import messageSlice from "./messageSlice";
 
 export const store = configureStore({
